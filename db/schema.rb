b0e1bb14226e51b6395bcae5f65e94e280529b29
Kohei SUGI <kohei.sg@gmail.com>
2020-08-09 02:30:46 +0900

    $ rails db:migrate

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 2020_08_08_172958) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "mama_contacts", force: :cascade do |t|
+    t.string "name"
+    t.string "key"
+    t.string "domain"
+    t.string "uuid"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
 end
