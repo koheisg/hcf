e160f4d1c295a99a424fa8d617e5674193c9f1d2
Kohei SUGI <kohei.sg@gmail.com>
2020-08-09 03:53:00 +0900

    Fix Sprockets::Rails::Helper::AssetNotPrecompiled error.

--- mama/lib/mama/engine.rb
+++ mama/lib/mama/engine.rb
@@ -1,5 +1,9 @@
 module Mama
   class Engine < ::Rails::Engine
     isolate_namespace Mama
+
+    initializer 'mama.assets.precompile' do |app|
+      app.config.assets.precompile += %w(mama/application.css)
+    end
   end
 end
